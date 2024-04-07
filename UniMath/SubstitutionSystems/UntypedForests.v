55398ff008e6296e5f133526d519221ebde7c7da
Jean <wontusemailcom@protonmail.com>
2024-04-07 15:37:32 +0200

    categorical view for Forests

--- UniMath/SubstitutionSystems/UntypedForests.v
+++ UniMath/SubstitutionSystems/UntypedForests.v
@@ -5,6 +5,7 @@ Require Import UniMath.MoreFoundations.Tactics.
 
 Require Import UniMath.Combinatorics.Lists.
 Require Import UniMath.Combinatorics.StandardFiniteSets.
+Require Import UniMath.Combinatorics.Vectors.
 
 Require Import UniMath.CategoryTheory.Core.Categories.
 Require Import UniMath.CategoryTheory.Core.Functors.
@@ -36,6 +37,7 @@ Require UniMath.SubstitutionSystems.MultiSortedMonadConstruction_alt.
 Require Import UniMath.SubstitutionSystems.MultiSortedMonadConstruction_actegorical.
 Require Import UniMath.SubstitutionSystems.MultiSortedMonadConstruction_coind_actegorical.
 Require Import UniMath.SubstitutionSystems.ContinuitySignature.InstantiateHSET.
+Require Import UniMath.SubstitutionSystems.MultiSortedEmbeddingIndCoindHSET.
 
 Local Open Scope cat.
 
@@ -47,9 +49,14 @@ Local Notation "[]" := (@nil _) (at level 0, format "[]").
 Local Notation "a + b" := (setcoprod a b) : set.
 Local Notation "'Id'" := (functor_identity _).
 
-Let sort : UU := stn 3.
+Definition sort : UU := stn 3.
 
+(* À prouver *)
+Local Definition  Hsort : isofhlevel 3 sort.
+Proof.
+Admitted.
 
+(* Voir comment contracter ces définitions *)
 Definition a : 0 < 3.
 Proof.
   simpl.
@@ -68,24 +75,83 @@ Proof.
   apply idpath.
 Qed.
 
-(* Sort des variables *)
+(* Sorte des variables *)
 Definition sv : sort := make_stn 3 0 a.
-(* Sort des termes *)
+(* Sorte des termes *)
 Definition st : sort := make_stn 3 0 b.
-(* Sort des sommes, ou eliminations alternatives *)
+(* Sorte des sommes, ou eliminations alternatives *)
 Definition se : sort := make_stn 3 0 c.
 
-Let sortToSet : UU := (sort -> hSet).
+Let sortToSet : category := SortIndexing.sortToSet sort Hsort.
+Let sortToSetSet : category := SortIndexing.sortToSetSet sort Hsort.
+Let sortToSet2 : category := SortIndexing.sortToSet2 sort Hsort.
 
-Definition UntypedForestSig : MultiSortedSig (⟦3⟧%stn).
+
+Let projSortToSet : sort -> sortToSetSet := MultiSortedMonadConstruction_alt.projSortToSet sort Hsort.
+Let projSortToSetvariable : (sort → sort) → sortToSet2 := projSortToCvariable sort Hsort HSET.
+Let hat_functorSet : sort -> HSET ⟶ sortToSet := MultiSortedMonadConstruction_alt.hat_functorSet sort Hsort.
+Let sorted_option_functorSet : sort → functor sortToSet sortToSet := MultiSortedMonadConstruction_alt.sorted_option_functorSet sort Hsort.
+
+Local Definition BCsortToSet : BinCoproducts sortToSet := SortIndexing.BCsortToSet sort Hsort.
+Local Definition BPsortToSet : BinProducts sortToSet := SortIndexing.BPsortToSet sort Hsort.
+
+Local Definition TsortToSet : Terminal sortToSet := SortIndexing.TsortToSet sort Hsort.
+Local Definition TsortToSet2 : Terminal sortToSet2 := SortIndexing.TsortToSet2 sort Hsort.
+
+Local Definition BPsortToSetSet : BinProducts sortToSetSet := SortIndexing.BPsortToSetSet sort Hsort.
+
+Local Lemma sortToSet2_comp_on_mor (F G : sortToSet2) {ξ ξ' : sortToSet} (f : sortToSet⟦ ξ, ξ' ⟧) (s : sort) (* (elem : pr1 (pr1 (pr1 (functor_compose F G) ξ) s)) *) :
+  pr1 (# (pr1 (functor_compose F G)) f) s = pr1 (# (pr1 G) (# (pr1 F) f)) s.
+Proof.
+  apply idpath.
+Qed.
+
+Lemma postcomp_with_projSortToSet_on_mor (F : sortToSet2) (s: sort) (ξ ξ' : sortToSet) (f : sortToSet ⟦ ξ, ξ' ⟧)
+  (arg : pr1 (pr1 (functor_compose F (projSortToSet s)) ξ))
+  : # (pr1 (functor_compose F (projSortToSet s))) f arg = pr1 (# (pr1 F) f) s arg.
+Proof.
+  apply idpath.
+Qed.
+
+Definition app_list_noind (n : nat) : list(list sort × sort).
+Proof.
+    use tpair.
+    - exact n.
+    - apply weqvecfun.
+      intro i. apply ( ([],,se) :: []).
+  Defined.
+
+
+Definition UntypedForest_Sig : MultiSortedSig (⟦3⟧%stn).
 Proof.
   use (make_MultiSortedSig (stn 3)).
   - apply (( (unit,,isasetunit) + (nat,,isasetnat) )%set).
     - intros H. induction H  as [abs | app].
       + exact ((((sv :: []) ,,st) :: []),,st).
-      + induction app.
-        * exact ((([] ,,sv ) :: nil),, se).
-        * exact ( (([],, sv) :: (pr1 IHapp))  ,, se).
+      + exact ( (app_list_noind  app) ,, st).
 Defined.
+
+(** The canonical functor associated with Untypedforest_Sig **)
+Definition Forest_Functor_H : functor sortToSet2 sortToSet2 :=
+  MultiSorted_actegorical.MultiSortedSigToFunctor' sort Hsort SET
+    TerminalHSET BinProductsHSET BinCoproductsHSET CoproductsHSET UntypedForest_Sig.
+
+
+(** the functor of which the fixed points are considered *)
+Definition Forest_Functor_Id_H : functor sortToSet2 sortToSet2 :=
+  SubstitutionSystems.Id_H sortToSet BCsortToSet Forest_Functor_H.
+
+(** the canonical strength associated with UntypedForest_Sig *)
+Let θForest := MultiSortedMonadConstruction_actegorical.MultiSortedSigToStrength' sort Hsort SET
+               TerminalHSET BinProductsHSET BinCoproductsHSET CoproductsHSET UntypedForest_Sig.
+
+Definition ctx_ext (ξ : sortToSet) (s : sort) : sortToSet
+  := sorted_option_functorSet s ξ.
+
+(** the sigma-monoids for wellfounded and non-wellfounded syntax for Forests *)
+Let σind : SigmaMonoid θForest := MultiSortedEmbeddingIndCoindHSET.σind sort Hsort UntypedForest_Sig.
+Let σcoind : SigmaMonoid θForest := MultiSortedEmbeddingIndCoindHSET.σcoind sort Hsort UntypedForest_Sig.
+
+
 
 End Signature.
