bd6de2ba831994e353e21d32b079ffd2f15b7a9a
ratpi fonctionnel <wontusemailcom@protonmail.com>
2024-06-19 17:34:32 +0200

    decoupage pour Down_works_with_ext + meilleures pratiques

--- UniMath/SubstitutionSystems/Forests.v
+++ UniMath/SubstitutionSystems/Forests.v
@@ -426,24 +426,22 @@ Proof.
 Defined.
 
 (* Montre que le changement de contexte est homogène *)
-Definition Down_Context_on_mor  {ξ ξ' : sortToSet}  (η : sortToSet⟦ξ,
-ξ'⟧) : UntypedForests.sortToSet⟦Down_Context ξ, Down_Context ξ'⟧.
+Definition Down_Context_on_mor  {ξ ξ' : sortToSet}  (η : sortToSet⟦ξ, ξ'⟧)
+  : UntypedForests.sortToSet⟦Down_Context ξ, Down_Context ξ'⟧.
 Proof.
-   apply nat_trans_functor_path_pregroupoid.
-   intros s.
-   use CoproductOfArrows.
-   intro c.
-   simpl.
-   apply η.
+  apply nat_trans_functor_path_pregroupoid.
+  intros s.
+  use CoproductOfArrows.
+  intro c.
+  simpl.
+  apply η.
 Defined.
 
 Definition Down_context_functor_data : functor_data sortToSet UntypedForests.sortToSet.
 Proof.
- use make_functor_data.
- - apply Down_Context.
- - intros a b f;
-   apply Down_Context_on_mor.
-   exact f.
+  use make_functor_data.
+  - apply Down_Context.
+  - intros a b f; exact (Down_Context_on_mor f).
 Defined.
 
 Lemma is_functor_down_context : is_functor Down_context_functor_data.
@@ -465,159 +463,156 @@ Proof.
     apply idpath.
 Qed.
 
-Definition Down_Context_functor : functor sortToSet UntypedForests.sortToSet := (Down_context_functor_data ,, is_functor_down_context).
+Definition Down_Context_functor : functor sortToSet UntypedForests.sortToSet
+  := (Down_context_functor_data ,, is_functor_down_context).
 
 Let option_fun_summand2 := option_fun_summand sort Hsort HSET TerminalHSET CoproductsHSET.
 
 Let option_fun_summand2_u := option_fun_summand UntypedForests.sort UntypedForests.Hsort HSET TerminalHSET CoproductsHSET.
 
-Definition Down_works_option_fun_summand_to (s : sort) (s0 :
-UntypedForests.sort) :
-   SET ⟦ pr1 (Down_Context (option_fun_summand2 s)) s0, pr1
-(option_fun_summand2_u (pr2 s)) s0 ⟧.
+Definition Down_works_option_fun_summand_to (s : sort) (s0 : UntypedForests.sort) :
+  SET ⟦ pr1 (Down_Context (option_fun_summand2 s)) s0, pr1 (option_fun_summand2_u (pr2 s)) s0 ⟧.
 Proof.
-   unfold Down_Context.
-   unfold functor_path_pregroupoid.
-   simpl.
-   intro x.
-   destruct x as [a [p q]].
-   rewrite <- p.
-   use tpair.
-   - apply idpath.
-   - exact q.
+  unfold Down_Context.
+  unfold functor_path_pregroupoid.
+  simpl.
+  intro x.
+  destruct x as [a [p q]].
+  rewrite <- p.
+  use tpair.
+  - apply idpath.
+  - exact q.
 Defined.
 
-Definition Down_works_option_fun_summand_from (s : sort) (s0 :
-UntypedForests.sort) :
-   SET ⟦ pr1 (option_fun_summand2_u (pr2 s)) s0, pr1 (Down_Context
-(option_fun_summand2 s)) s0 ⟧.
+Definition Down_works_option_fun_summand_from (s : sort) (s0 : UntypedForests.sort) :
+  SET ⟦ pr1 (option_fun_summand2_u (pr2 s)) s0, pr1 (Down_Context (option_fun_summand2 s)) s0 ⟧.
 Proof.
-   simpl.
-   intro x.
-   destruct x as [p q].
-   exists (pr1 s).
-   rewrite  p.
-   use tpair.
-   - apply idpath.
-   - exact q.
+  simpl.
+  intro x.
+  destruct x as [p q].
+  exists (pr1 s).
+  rewrite  p.
+  use tpair.
+  - apply idpath.
+  - exact q.
 Defined.
 
-Lemma Down_works_option_fun_summand_aux (s : sort) (s0 :
-UntypedForests.sort) :
-   is_inverse_in_precat (Down_works_option_fun_summand_to s s0)
-(Down_works_option_fun_summand_from s s0).
+Lemma Down_works_option_fun_summand_aux (s : sort) (s0 : UntypedForests.sort) :
+  is_inverse_in_precat (Down_works_option_fun_summand_to s s0)
+                       (Down_works_option_fun_summand_from s s0).
 Proof.
-   split.
-   - apply funextfun.
-     intro x.
-     destruct x as [a [p q]].
-     cbn.
-     use total2_paths_f.
-     + simpl.
-       rewrite <- p.
-       apply idpath.
-     + use total2_paths_f.
-       * apply Hsort'.
-       * apply isapropunit.
-   - apply funextfun.
-     intro x.
-     destruct x as [p q].
-     cbn.
-     use total2_paths_f.
-     + apply (setproperty (stnset 3)).
-     + apply isapropunit.
+  split.
+  - apply funextfun.
+    intro x.
+    destruct x as [a [p q]].
+    cbn.
+    use total2_paths_f.
+    + simpl.
+      rewrite <- p.
+      apply idpath.
+    + use total2_paths_f.
+      * apply Hsort'.
+      * apply isapropunit.
+  - apply funextfun.
+    intro x.
+    destruct x as [p q].
+    cbn.
+    use total2_paths_f.
+    + apply (setproperty (stnset 3)).
+    + apply isapropunit.
 Qed.
 
-
 Definition Down_works_option_fun_summand (s : sort) :
-   UntypedForests.ctx_equiv
-     (Down_Context (option_fun_summand2 s))
-     (option_fun_summand2_u (pr2 s)).
+  UntypedForests.ctx_equiv
+    (Down_Context (option_fun_summand2 s))
+    (option_fun_summand2_u (pr2 s)).
 Proof.
-   red.
-   intros s0.
-   use make_z_iso.
-   - apply Down_works_option_fun_summand_to.
-   - apply Down_works_option_fun_summand_from.
-   - apply Down_works_option_fun_summand_aux.
+  red.
+  intros s0.
+  use make_z_iso.
+  - apply Down_works_option_fun_summand_to.
+  - apply Down_works_option_fun_summand_from.
+  - apply Down_works_option_fun_summand_aux.
 Defined.
 
-Definition Down_works_with_ext_to (ξ : sortToSet) (s : sort) (s0 :
-UntypedForests.sort) :
-   SET ⟦ pr1 (Down_Context (ctx_ext ξ s)) s0, pr1
-(UntypedForests.ctx_ext (Down_Context ξ) (pr2 s)) s0 ⟧.
+Definition Down_works_with_ext_to (ξ : sortToSet) (s : sort) (s0 : UntypedForests.sort) :
+  SET ⟦ pr1 (Down_Context (ctx_ext ξ s)) s0, pr1 (UntypedForests.ctx_ext (Down_Context ξ) (pr2 s)) s0 ⟧.
 Proof.
-   intro x.
-   destruct x as [a x'].
-   simpl in x'.
-   destruct x' as [option | base].
-   - apply ii1.
-     exact (pr1 ( Down_works_option_fun_summand s s0 ) (a ,, option)).
-   - apply ii2.
-     simpl.
-     exists a.
-     exact base.
+  intro x.
+  destruct x as [a x'].
+  simpl in x'.
+  destruct x' as [option | base].
+  - apply ii1.
+    exact (pr1 (Down_works_option_fun_summand s s0) (a ,, option)).
+  - apply ii2.
+    simpl.
+    exists a.
+    exact base.
 Defined.
 
-Definition Down_works_with_ext_from (ξ : sortToSet) (s : sort) (s0 :
-UntypedForests.sort) :
-   SET ⟦ pr1 (UntypedForests.ctx_ext (Down_Context ξ) (pr2 s)) s0, pr1
-(Down_Context (ctx_ext ξ s)) s0 ⟧.
+Definition Down_works_with_ext_from (ξ : sortToSet) (s : sort) (s0 : UntypedForests.sort) :
+  SET ⟦ pr1 (UntypedForests.ctx_ext (Down_Context ξ) (pr2 s)) s0, pr1 (Down_Context (ctx_ext ξ s)) s0 ⟧.
 Proof.
-   intro x.
-   destruct x as [option | base].
-   - simpl in option.
-     simpl.
-     exists (pr1 s).
-     apply ii1.
-     set (auxarg := pr1 (pr1 (option_fun_summand2_u (pr2 s)) s0)).
-     simpl in auxarg.
-     set (aux := pr12 (Down_works_option_fun_summand s s0) option).
-     simpl in aux.
-     destruct aux as [a [p q]].
-     use tpair.
-     + rewrite <- p. simpl. apply idpath.
-     + exact q.
-   - simpl in base.
-     destruct base as [a  t].
-     simpl.
-     exists a.
-     apply ii2.
-     exact t.
+  intro x.
+  destruct x as [option | base].
+  - simpl in option.
+    simpl.
+    exists (pr1 s).
+    apply ii1.
+    set (auxarg := pr1 (pr1 (option_fun_summand2_u (pr2 s)) s0)).
+    simpl in auxarg.
+    set (aux := pr12 (Down_works_option_fun_summand s s0) option).
+    simpl in aux.
+    destruct aux as [a [p q]].
+    use tpair.
+    + rewrite <- p. simpl. apply idpath.
+    + exact q.
+  - simpl in base.
+    destruct base as [a  t].
+    simpl.
+    exists a.
+    apply ii2.
+    exact t.
 Defined.
 
-Lemma Down_works_with_ext (ξ : sortToSet) ( s : sort) :
-   UntypedForests.ctx_equiv
-     (Down_Context (ctx_ext ξ s))
-     (UntypedForests.ctx_ext (Down_Context ξ) (pr2 s)).
+Lemma Down_works_with_ext_aux (ξ : sortToSet) (s : sort) (s0 : UntypedForests.sort) :
+  is_inverse_in_precat (Down_works_with_ext_to ξ s s0) (Down_works_with_ext_from ξ s s0).
+Proof.
+  split.
+  - apply funextfun.
+    intro y.
+    simpl.
+    destruct y as [ot b].
+    destruct b as [f1 | f2].
+    + destruct f1 as [p q].
+      use total2_paths_f.
+      * cbn.
+        apply (maponpaths pr1) in p.
+        exact (!p).
+      * induction p.
+        apply idpath.
+    + cbn. apply idpath.
+  - apply funextfun.
+    intro x.
+    destruct x as [p | q] ; cbn.
+    + apply maponpaths.
+      use total2_paths_f.
+      * apply UntypedForests.Hsort'.
+      * apply isProofIrrelevantUnit.
+    + apply idpath.
+Qed.
+
+Lemma Down_works_with_ext (ξ : sortToSet) (s : sort) :
+  UntypedForests.ctx_equiv
+    (Down_Context (ctx_ext ξ s))
+    (UntypedForests.ctx_ext (Down_Context ξ) (pr2 s)).
 Proof.
    red.
    intros s0.
    use make_z_iso.
    - apply Down_works_with_ext_to.
    - apply Down_works_with_ext_from.
-   - split.
-     + apply funextfun.
-       intro y.
-       simpl.
-       destruct y as [ot b].
-       destruct b as [f1 | f2].
-       * destruct f1 as [p q].
-         use total2_paths_f.
-         -- cbn.
-            apply (maponpaths pr1) in p.
-            exact (!p).
-         -- induction p.
-            apply idpath.
-       * cbn. apply idpath.
-   + apply funextfun.
-     intro x.
-     destruct x as [p | q] ; cbn.
-     * apply maponpaths.
-       use total2_paths_f.
-       -- apply UntypedForests.Hsort'.
-       -- apply isProofIrrelevantUnit.
-     * apply idpath.
+   - apply Down_works_with_ext_aux.
 Defined.
 
 (*
@@ -625,48 +620,44 @@ On veut créer un foncteur entre les sortToSet
 *)
 Definition Carrier_detype_data_ind : functor_data sortToSet sortToSet.
 Proof.
-   use make_functor_data.
-   - intros ξ.
-     apply functor_path_pregroupoid.
-     intros s.
-     exact (pr1 (pr1 UntypedForests.UntypedForest_ind (Down_Context ξ))
-(pr2 s)).
-   - intros ξ ξ' η.
-     apply nat_trans_functor_path_pregroupoid.
-     intros s.
-     change (pr1 (pr1 (pr1 UntypedForests.UntypedForest_ind
-(Down_Context ξ)) (pr2 s)) ->
-             pr1 (pr1 (pr1 UntypedForests.UntypedForest_ind
-(Down_Context ξ')) (pr2 s))).
-     set (aux := # (pr1 UntypedForests.UntypedForest_ind)
-(Down_Context_on_mor η)).
-     (* On applique le T^u  à la transformation naturelle de ξ en ξ' *)
-     exact (pr1 aux (pr2 s)).
+  use make_functor_data.
+  - intros ξ.
+    apply functor_path_pregroupoid.
+    intros s.
+    exact (pr1 (pr1 UntypedForests.UntypedForest_ind (Down_Context ξ)) (pr2 s)).
+  - intros ξ ξ' η.
+    apply nat_trans_functor_path_pregroupoid.
+    intros s.
+    change (pr1 (pr1 (pr1 UntypedForests.UntypedForest_ind (Down_Context ξ)) (pr2 s)) ->
+            pr1 (pr1 (pr1 UntypedForests.UntypedForest_ind (Down_Context ξ')) (pr2 s))).
+    set (aux := # (pr1 UntypedForests.UntypedForest_ind) (Down_Context_on_mor η)).
+    (* On applique le T^u  à la transformation naturelle de ξ en ξ' *)
+    exact (pr1 aux (pr2 s)).
 Defined.
 
 Lemma Carrier_detype_data_ind_is_functor : is_functor Carrier_detype_data_ind.
 Proof.
-split ; red.
-- intro ξ.
-  apply nat_trans_eq.
-  exact has_homsets_HSET.
-  intro x.
-  apply funextfun.
-  intro elem.
-  unfold functor_on_morphisms.
-  unfold Carrier_detype_data_ind.
-  unfold make_functor_data.
-  unfold nat_trans_functor_path_pregroupoid.
-  unfold make_nat_trans.
-  unfold nat_trans_data_from_nat_trans_funclass.
-  unfold pr1. unfold  pr2.
-  change (Down_Context_on_mor (identity ξ)) with (# Down_Context_functor (identity ξ)).
-  rewrite (functor_id).
-  rewrite (functor_id  UntypedForests.UntypedForest_ind).
-  apply idpath.
+  split; red.
+  - intro ξ.
+    apply nat_trans_eq.
+    { exact has_homsets_HSET. }
+    intro x.
+    apply funextfun.
+    intro elem.
+    unfold functor_on_morphisms.
+    unfold Carrier_detype_data_ind.
+    unfold make_functor_data.
+    unfold nat_trans_functor_path_pregroupoid.
+    unfold make_nat_trans.
+    unfold nat_trans_data_from_nat_trans_funclass.
+    unfold pr1. unfold pr2.
+    change (Down_Context_on_mor (identity ξ)) with (# Down_Context_functor (identity ξ)).
+    rewrite (functor_id).
+    rewrite (functor_id UntypedForests.UntypedForest_ind).
+    apply idpath.
   - intros ξ1 ξ2 ξ3 f g.
     apply nat_trans_eq.
-    exact has_homsets_HSET.
+    { exact has_homsets_HSET. }
     intro x.
     apply funextfun.
     intro elem.
@@ -683,31 +674,31 @@ split ; red.
     apply idpath.
 Qed.
 
-Definition Carrier_detype_ind : sortToSet2 := Carrier_detype_data_ind ,,
-Carrier_detype_data_ind_is_functor.
+Definition Carrier_detype_ind : sortToSet2 :=
+  Carrier_detype_data_ind ,, Carrier_detype_data_ind_is_functor.
 
-Definition Detype_ind_alg_data : nat_trans_data (pr1
-(Forest_Functor_Id_H Carrier_detype_ind)) (pr1 Carrier_detype_ind).
+Definition Detype_ind_alg_data
+  : nat_trans_data (pr1 (Forest_Functor_Id_H Carrier_detype_ind)) (pr1 Carrier_detype_ind).
 Proof.
-   intro ξ.
-   apply nat_trans_functor_path_pregroupoid.
-   intro s.
-   use BinCoproductArrow.
-   - change (SET⟦ pr1 ξ s, pr1 (pr1 Carrier_detype_ind ξ) s ⟧).
-     intro x.
-     refine (pr1 (pr1 UntypedForests.UntypedForest_eta_ind _) _ _).
-     exists (pr1 s).
-     exact x.
-   - use CoproductArrow.
-     intro op.
-     change (SET⟦
-       pr1 (pr1
-(ContinuityOfMultiSortedSigToFunctor.hat_exp_functor_list'_optimized sort
-         Hsort SET TerminalHSET BinProductsHSET BinCoproductsHSET
-CoproductsHSET
-         (arity sort Forest_Sig op) Carrier_detype_ind) ξ) s,
-       pr1 (pr1 Carrier_detype_ind ξ) s ⟧).
-     admit.
+  intro ξ.
+  apply nat_trans_functor_path_pregroupoid.
+  intro s.
+  use BinCoproductArrow.
+  - change (SET⟦ pr1 ξ s, pr1 (pr1 Carrier_detype_ind ξ) s ⟧).
+    intro x.
+    refine (pr1 (pr1 UntypedForests.UntypedForest_eta_ind _) _ _).
+    exists (pr1 s).
+    exact x.
+  - use CoproductArrow.
+    intro op.
+    change (SET⟦
+                pr1 (pr1
+                       (ContinuityOfMultiSortedSigToFunctor.hat_exp_functor_list'_optimized sort
+                          Hsort SET TerminalHSET BinProductsHSET BinCoproductsHSET
+                          CoproductsHSET
+                          (arity sort Forest_Sig op) Carrier_detype_ind) ξ) s,
+                pr1 (pr1 Carrier_detype_ind ξ) s ⟧).
+    admit.
 Admitted.
 
 Lemma Detype_ind_alg_data_is_nat_trans : is_nat_trans _ _
@@ -717,73 +708,70 @@ Admitted.
 
 Definition Detype_ind_alg : FunctorAlg Forest_Functor_Id_H.
 Proof.
-   use tpair.
-   - exact Carrier_detype_ind.
-   - change (Forest_Functor_Id_H Carrier_detype_ind --> Carrier_detype_ind).
-     exact (Detype_ind_alg_data ,, Detype_ind_alg_data_is_nat_trans).
+  use tpair.
+  - exact Carrier_detype_ind.
+  - change (Forest_Functor_Id_H Carrier_detype_ind --> Carrier_detype_ind).
+    exact (Detype_ind_alg_data ,, Detype_ind_alg_data_is_nat_trans).
 Defined.
 
 Definition Detype_ind : Forest_ind --> Carrier_detype_ind.
 Proof.
-   exact (pr1 (InitialArrow Forest_ind_IA Detype_ind_alg)).
+  exact (pr1 (InitialArrow Forest_ind_IA Detype_ind_alg)).
 Defined.
 
 Definition Down_ops : ops _ Forest_Sig -> ops _
 UntypedForests.UntypedForest_Sig.
 Proof.
-   intro op.
-   induction op  as [term_construct | elim_construct].
-   + induction term_construct as [abs|sum].
-     * exact (inl (inl tt)).
-     * induction sum as [p n].
-       exact (inl (inr n)).
-   + induction elim_construct as [B p].
-     exact (inr (length B)).
+  intro op.
+  induction op  as [term_construct | elim_construct].
+  + induction term_construct as [abs|sum].
+    * exact (inl (inl tt)).
+    * induction sum as [p n].
+      exact (inl (inr n)).
+  + induction elim_construct as [B p].
+    exact (inr (length B)).
 Defined.
 
 Definition Carrier_detype_data_coind : functor_data sortToSet sortToSet.
 Proof.
-   use make_functor_data.
-   - intros ξ.
-     apply functor_path_pregroupoid.
-     intros s.
-     exact (pr1 (pr1 UntypedForests.UntypedForest_coind (Down_Context ξ))
-(pr2 s)).
-   - intros ξ ξ' η.
-     apply nat_trans_functor_path_pregroupoid.
-     intros s.
-     change (pr1 (pr1 (pr1 UntypedForests.UntypedForest_coind
-(Down_Context ξ)) (pr2 s)) ->
-             pr1 (pr1 (pr1 UntypedForests.UntypedForest_coind
-(Down_Context ξ')) (pr2 s))).
-     set (aux := # (pr1 UntypedForests.UntypedForest_coind)
-(Down_Context_on_mor η)).
-     exact (pr1 aux (pr2 s)).
+  use make_functor_data.
+  - intros ξ.
+    apply functor_path_pregroupoid.
+    intros s.
+    exact (pr1 (pr1 UntypedForests.UntypedForest_coind (Down_Context ξ)) (pr2 s)).
+  - intros ξ ξ' η.
+    apply nat_trans_functor_path_pregroupoid.
+    intros s.
+    change (pr1 (pr1 (pr1 UntypedForests.UntypedForest_coind (Down_Context ξ)) (pr2 s)) ->
+            pr1 (pr1 (pr1 UntypedForests.UntypedForest_coind (Down_Context ξ')) (pr2 s))).
+    set (aux := # (pr1 UntypedForests.UntypedForest_coind)
+                  (Down_Context_on_mor η)).
+    exact (pr1 aux (pr2 s)).
 Defined.
 
 Lemma Carrier_detype_data_coind_is_functor : is_functor Carrier_detype_data_coind.
 Proof.
   split ; red.
-- intro ξ.
-  apply nat_trans_eq.
-  exact has_homsets_HSET.
-  intro x.
-  apply funextfun.
-  intro elem.
-  unfold functor_on_morphisms.
-  unfold Carrier_detype_data_coind.
-  unfold make_functor_data.
-  unfold nat_trans_functor_path_pregroupoid.
-  unfold make_nat_trans.
-  unfold nat_trans_data_from_nat_trans_funclass.
-  unfold pr1. unfold  pr2.
-  change (Down_Context_on_mor (identity ξ)) with (# Down_Context_functor (identity ξ)).
-  rewrite (functor_id).
-  rewrite (functor_id UntypedForests.UntypedForest_coind).
-  apply idpath.
+  - intro ξ.
+    apply nat_trans_eq.
+    { exact has_homsets_HSET. }
+    intro x.
+    apply funextfun.
+    intro elem.
+    unfold functor_on_morphisms.
+    unfold Carrier_detype_data_coind.
+    unfold make_functor_data.
+    unfold nat_trans_functor_path_pregroupoid.
+    unfold make_nat_trans.
+    unfold nat_trans_data_from_nat_trans_funclass.
+    unfold pr1. unfold pr2.
+    change (Down_Context_on_mor (identity ξ)) with (# Down_Context_functor (identity ξ)).
+    rewrite (functor_id).
+    rewrite (functor_id UntypedForests.UntypedForest_coind).
+    apply idpath.
   - intros ξ1 ξ2 ξ3 f g.
     apply nat_trans_eq.
-    exact has_homsets_HSET.
+    { exact has_homsets_HSET. }
     intro x.
     apply funextfun.
     intro elem.
@@ -800,8 +788,8 @@ Proof.
     apply idpath.
 Qed.
 
-Definition Carrier_detype_coind : sortToSet2 := Carrier_detype_data_ind ,,
-Carrier_detype_data_ind_is_functor.
+Definition Carrier_detype_coind : sortToSet2
+  := Carrier_detype_data_ind ,, Carrier_detype_data_ind_is_functor.
 
 End Typing.
 
